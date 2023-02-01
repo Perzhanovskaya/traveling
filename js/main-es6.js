5d91ba54c5203bc76f198831f8f084467e1205b0
Elizabeth Perzhanovskaya <e.perzhanovskaya@gmail.com>
2023-02-01 20:20:57 +0100

    optimization

--- js/main-es6.js
+++ js/main-es6.js
@@ -15,12 +15,20 @@
     const menuOpen = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuClose = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     menuOpen.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
     });
     menuClose.addEventListener('click', () => {
         menu.classList.remove('header__nav_active');
     });
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav_active');
+            });
+        }
+    }
 }());
 
 //scroll
